fa9b21ecabb36e28d2fe46eddf4afc651b85b0d6
L.P <parisot.laurent@gmail.com>
2017-10-16 17:00:24 +0200

    Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,7 +12,7 @@ import { LeaderProvider } from '../../providers/leader/leader';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
   dish: Dish;
   promotion: Promotion;
   leader: Leader;
